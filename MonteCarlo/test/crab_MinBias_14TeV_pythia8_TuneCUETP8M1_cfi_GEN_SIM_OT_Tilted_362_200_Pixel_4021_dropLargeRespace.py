8e80201a2494e22749a657dc628e1603c01f09c5
Juliette Alimena <juliette.alimena@cern.ch>
2017-02-23 02:39:42 -0600

    change to more descriptive dataset name

--- MonteCarlo/test/crab_MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py
+++ MonteCarlo/test/crab_MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'MinBias_dropLargeRespace'
+config.General.requestName = 'MinBias_14TeV_GenSim_dropLargeRespace'
 config.General.workArea = 'crab'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -9,7 +9,7 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = 'MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py'
 
-config.Data.outputPrimaryDataset = 'MinBias_dropLargeRespace'
+config.Data.outputPrimaryDataset = 'MinBias_14TeV_GenSim_dropLargeRespace'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 10
 NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
