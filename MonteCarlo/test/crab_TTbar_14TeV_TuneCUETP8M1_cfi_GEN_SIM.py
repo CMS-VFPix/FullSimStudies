899d3d78db570450c350d8f3b699d4414805217f
Juliette Alimena <juliette.alimena@cern.ch>
2017-03-14 15:37:59 -0500

    add crab configs for ttbar with D4 and modified geometry: step1, step2 PU140

---
+++ MonteCarlo/test/crab_TTbar_14TeV_TuneCUETP8M1_cfi_GEN_SIM.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'TTbar_14TeV_GEN_SIM_2023D4_900pre4'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'TTbar_14TeV_TuneCUETP8M1_cfi_GEN_SIM.py'
+
+config.Data.outputPrimaryDataset = 'TTbar_14TeV_2023D4_900pre4'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/group/lpcfpix'
+config.Data.publication = True
+config.Site.storageSite = 'T3_US_FNALLPC'
