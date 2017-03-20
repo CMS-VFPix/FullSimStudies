74aa2f999cf3690989f76ccce7b325d6f10063c6
Juliette Alimena <juliette.alimena@cern.ch>
2017-03-20 11:33:07 -0500

    increase number of events, add tag to clarify sample

--- MonteCarlo/test/crab_MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py
+++ MonteCarlo/test/crab_MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'MinBias_14TeV_GenSim_dropLargeRespace'
+config.General.requestName = 'MinBias_14TeV_GenSim_dropLargeRespace_duplicateMaterialRemoved'
 config.General.workArea = 'crab'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -9,10 +9,10 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = 'MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT_Tilted_362_200_Pixel_4021_dropLargeRespace.py'
 
-config.Data.outputPrimaryDataset = 'MinBias_14TeV_GenSim_dropLargeRespace'
+config.Data.outputPrimaryDataset = 'MinBias_14TeV_GenSim_dropLargeRespace_duplicateMaterialRemoved'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 10
-NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+NJOBS = 1000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/group/lpcfpix'
 config.Data.publication = True
