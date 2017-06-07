e5736cc5cbe24617ef4bf5d77707eb2189e7208e
Juliette Alimena <juliette.alimena@cern.ch>
2017-06-07 12:10:48 -0500

    add gen-sim MinBias for D17 and modified geometries

---
+++ MonteCarlo/test/crab_MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT613_200_IT4025_opt8s3l.py
@@ -0,0 +1,21 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'MinBias_14TeV_GenSim_OT613_200_IT4025_opt8s3l'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'MinBias_14TeV_pythia8_TuneCUETP8M1_cfi_GEN_SIM_OT613_200_IT4025_opt8s3l.py'
+
+config.Data.outputPrimaryDataset = 'MinBias_14TeV_921_OT613_200_IT4025_opt8s3l'
+config.Data.outputDatasetTag = 'GenSim'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 100
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/group/lpcfpix'
+config.Data.publication = True
+
+config.Site.storageSite = 'T3_US_FNALLPC'
