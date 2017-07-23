06fd877bc06b588e16d79ea285f896078ddb6e28
Juliette Alimena <juliette.alimena@cern.ch>
2017-07-23 01:51:35 -0500

    add step3 ttbar full reco opt6s3l

---
+++ MonteCarlo/test/crab_step3_TTbar_OT613_200_IT4025_opt6s3l.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'TTbar_14TeV_step3_923_PU200_OT613_200_IT4025_opt6s3l'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step3_RAW2DIGI_RECO_VALIDATION_DQM_PU200_OT613_200_IT4025_opt6s3l.py'
+config.JobType.maxMemoryMB = 8000
+
+config.Data.inputDataset = '/TTbar_14TeV_923_OT613_200_IT4025_opt6s3l/jalimena-step2_PU200-8e71eb3dbd7c57ddd1280ba82ed284d3/USER'
+config.Data.outputDatasetTag = 'step3_PU200'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/group/lpcfpix'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
