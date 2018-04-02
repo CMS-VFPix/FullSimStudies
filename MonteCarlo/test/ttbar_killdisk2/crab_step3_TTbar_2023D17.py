19b24a99b130c332774f96a5050490a37d2832fd
Juliette Alimena <juliette.alimena@cern.ch>
2018-04-02 06:09:04 -0500

    dead FPix2_pos step3, PU200 for our private 4025 geometry

--- MonteCarlo/test/ttbar_killdisk2/crab_step3_TTbar_2023D17.py
+++ MonteCarlo/test/ttbar_killdisk2/crab_step3_TTbar_2023D17.py
@@ -2,17 +2,18 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 
-config.General.requestName = 'TTbar_14TeV_step3_932_PU200_2023D17'
+config.General.requestName = 'TTbar_14TeV_step3_932_PU200_2023D17_OT613_IT4025'
 config.General.workArea = 'crab'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'step3_RAW2DIGI_L1Reco_RECO_PAT_VALIDATION_DQM_PU200.py'
+config.JobType.psetName = 'step3_RAW2DIGI_L1Reco_RECO_PAT_VALIDATION_DQM_PU200_OT613_IT4025.py'
 config.JobType.maxMemoryMB = 8000
+config.JobType.numCores = 4
 
-config.Data.inputDataset = ''
-config.Data.outputDatasetTag = 'step3_PU200'
+config.Data.inputDataset = '/TTbar_14TeV_932_OT613_200_IT4025/jalimena-step2_PU200_deadFPix2pos-15b244caa00a89f5ae32cac14dfe5cce/USER'
+config.Data.outputDatasetTag = 'step3_PU200_deadFPix2pos_noTrackingChange'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
