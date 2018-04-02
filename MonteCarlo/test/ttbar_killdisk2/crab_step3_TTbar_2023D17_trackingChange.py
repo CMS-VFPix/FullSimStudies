1a714e644b1712a6d11b427fcba1a6cb74e217b0
Juliette Alimena <juliette.alimena@cern.ch>
2018-04-02 09:44:15 -0500

    add crab config for step3 with modified tracking seeding

---
+++ MonteCarlo/test/ttbar_killdisk2/crab_step3_TTbar_2023D17_trackingChange.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'TTbar_14TeV_step3_932_PU200_2023D17_OT613_IT4025'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step3_RAW2DIGI_L1Reco_RECO_PAT_VALIDATION_DQM_PU200_OT613_IT4025.py'
+config.JobType.maxMemoryMB = 16000
+config.JobType.numCores = 4
+
+config.Data.inputDataset = '/TTbar_14TeV_932_OT613_200_IT4025/jalimena-step2_PU200_deadFPix2pos-15b244caa00a89f5ae32cac14dfe5cce/USER'
+config.Data.outputDatasetTag = 'step3_PU200_deadFPix2pos_modTrackingSeeding'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/group/lpcfpix'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
