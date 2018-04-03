4b6ff8a37548c558eea6362f04687d43c65b3f4d
Susan Dittmer <sjd237@cornell.edu>
2018-04-03 15:16:05 +0200

    Crab / CMSSW configs for rad damage studies

---
+++ MonteCarlo/test/ttbar_radStudies/crab_step2_TTbar_2023D17_rad12.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+
+config.General.requestName = 'TTbar_14TeV_step2_932_PU200_OT613_IT4025_rad12'
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'step2_DIGI_L1_L1TrackTrigger_DIGI2RAW_HLT_PU200_OT613_IT4025.py'
+config.JobType.sendPythonFolder = True
+config.JobType.maxMemoryMB = 4000
+
+config.Data.inputDataset = '/TTbar_14TeV_932_OT613_200_IT4025/jalimena-GenSim-07a47ef0959d9ba314cf0f592af5aa44/USER'
+config.Data.outputDatasetTag = 'step2_PU200_rad12'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/group/lpcfpix'
+config.Data.publication = True
+config.Data.ignoreLocality = True
+
+config.Site.whitelist = ["T1_US_FNAL"]
+config.Site.storageSite = 'T3_US_FNALLPC'
