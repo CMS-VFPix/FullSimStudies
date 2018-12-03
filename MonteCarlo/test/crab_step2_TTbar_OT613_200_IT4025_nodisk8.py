5a4ef18f476dfa638ce1834333653cafadd273e2
Juliette Alimena <juliette.alimena@cern.ch>
2018-12-03 06:11:40 -0600

    add input dataset

--- MonteCarlo/test/crab_step2_TTbar_OT613_200_IT4025_nodisk8.py
+++ MonteCarlo/test/crab_step2_TTbar_OT613_200_IT4025_nodisk8.py
@@ -11,7 +11,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step2_DIGI_L1_L1TrackTrigger_DIGI2RAW_HLT_PU200_OT613_200_IT4025_nodisk8.py'
 config.JobType.maxMemoryMB = 4000
 
-config.Data.inputDataset = ''
+config.Data.inputDataset = '/TTbar_14TeV_923_OT613_200_IT4025_nodisk8/jalimena-GenSim-0c95d68a18eb4b58258e44e1d2e85101/USER'
 config.Data.outputDatasetTag = 'step2_PU200'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
