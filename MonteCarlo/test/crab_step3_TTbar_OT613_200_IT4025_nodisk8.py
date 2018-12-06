4260dd7b7007d1fedc8ecd02977eea5ee9a2a15d
Juliette Alimena <juliette.alimena@cern.ch>
2018-12-06 04:06:55 -0600

    update input dataset

--- MonteCarlo/test/crab_step3_TTbar_OT613_200_IT4025_nodisk8.py
+++ MonteCarlo/test/crab_step3_TTbar_OT613_200_IT4025_nodisk8.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 
-config.General.requestName = 'TTbar_14TeV_step3_921_PU200_OT613_200_IT4025_nodisk8'
+config.General.requestName = 'TTbar_14TeV_step3_923_PU200_OT613_200_IT4025_nodisk8'
 config.General.workArea = 'crab'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -11,7 +11,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step3_RAW2DIGI_RECO_VALIDATION_DQM_PU200_OT613_200_IT4025_nodisk8.py'
 config.JobType.maxMemoryMB = 8000
 
-config.Data.inputDataset = ''
+config.Data.inputDataset = '/TTbar_14TeV_923_OT613_200_IT4025_nodisk8/jalimena-step2_PU200-9c9fb876b8db36d0753875cab0de2080/USER'
 config.Data.outputDatasetTag = 'step3_PU200'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
